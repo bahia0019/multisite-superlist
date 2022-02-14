42590b730aceb5bc1a1bef0205ab12d710048fca
William Bay <william@flauntyoursite.com>
2022-02-13 20:33:08 -0800

    Using React and the REST api to create a brand new experience.

--- msl/src/Sitelist.js
+++ msl/src/Sitelist.js
@@ -8,34 +8,20 @@ import axios from "axios";
 const mslSiteInfo = msl_site_info
 const networkURL =  `${mslSiteInfo.site_url}/wp-admin/network/`
 const sitesEndpoint = `${mslSiteInfo.site_url}/wp-json/msl/v1/sites`
-
-const TOTAL_PAGES = 30;
 const mySites = document.querySelector('#wp-admin-bar-my-sites a');
+// TODO Pagination const TOTAL_PAGES = 30;
 
-// The loading spinner.
-const Loader = () => {
-  return (
-      <div >
-          <svg class="animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
-      </div>
-  )
-}
-
-// ****************
-// Setting up the initial load when mouse hovers "My Sites".
-function hover() {
-  mySites.addEventListener("mouseenter", 
-    function(){
-      console.log("poop");
-    }
-  )
-}
-hover()
-//************** */
-
+// TODO The loading spinner.
+// const Loader = () => {
+//   return (
+//       <div >
+//           <svg class="animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+//               <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+//               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+//           </svg>
+//       </div>
+//   )
+// }
 
 
 // Outputs the site list.
@@ -83,10 +69,10 @@ function Sitelist () {
               site => {
                 return (
                   <li key={site.site_id} class="site">
-                    <img src={site.favicon} id="msl-favicon" alt="poop"/> 
-                    <a href={site.url} id="msl-sitename" >{site.name}</a>
-                    <a href={site.url + '/wp-admin'}>Dashboard</a>
-                    <a href={site.url}>Visit Site</a>
+                    <img src={site.favicon} id="msl-favicon" alt=""/> 
+                    <a href={site.url + '/'} id="msl-sitename" >{site.name}</a>
+                    <a href={site.url + '/wp-admin/'}>Dashboard</a>
+                    <a href={site.url + '/'}>Visit Site</a>
                     <a href={site.url + '/wp-admin/post-new.php'}>New Post</a>
                     <a href={site.url + '/wp-admin/post-new.php?post_type=page'}>New Page</a>
                     <a href={site.url + '/wp-admin/plugins.php'}>Plugins</a>
@@ -95,7 +81,7 @@ function Sitelist () {
               }
             )}
         </ul>
-        {isLoading && <Loader />}
+        {/* TODO {isLoading && <Loader />} */}
       </div>
     </Fragment>
   );
--- multisite-sites-list.php
+++ multisite-sites-list.php
@@ -4,119 +4,119 @@
  * Plugin Name:       Multisites Sites List
  * GitHub Plugin URI: https://github.com/bahia0019/multisite-sites-list
  * Description:       Replaces My Sites list with a scrollable/searchable list of Sites.
- * Version:           1.1.0
+ * Version:           2.0.0
  * Author:            William Bay | Flaunt Your Site
  * Author URI:        http://flauntyoursite.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       multisite-sites-list
  */
-
+ 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// function check_user() {
-// if ( ! is_user_logged_in() || ! is_multisite() ) {
-// return;
-// }
-// if ( ! is_countable( $wp_admin_bar->user->blogs ) && count( $wp_admin_bar->user->blogs ) < 1 || ( ! current_user_can( 'manage_network' ) ) ) {
-// return;
-// }
-// }
-// add_action( 'init', 'check_user' );
+$version = '2.0.0';
 
 /**
- * Enqueues React static file for the App.
+ * Checks user to determine if plugin should run.
  */
-function msl_scripts() {
+function check_user() {
 
-	// Build files.
-	$react_app_js  = plugin_dir_url( __FILE__ ) . 'msl/build/static/js/all_in_one_file.js';
-	$react_app_css = plugin_dir_url( __FILE__ ) . 'msl/build/static/css/all_in_one_file.css';
-
-	wp_enqueue_style( 'msl-styles', $react_app_css, array(), date( 'YmdHis' ) );
-	wp_enqueue_script( 'msl-js', $react_app_js, array(), date( 'YmdHis' ), true );
-
-	wp_localize_script(
-		'msl-js',
-		'msl_site_info',
-		array(
-			'site_url' => site_url(),
-		)
-	);
-}
-add_action( 'admin_enqueue_scripts', 'msl_scripts' );
-
-
-/**
- * Removes items from the site list items.
- */
-function msl_remove_nodes( $wp_admin_bar ) {
-
-	$sites = get_sites();
-	foreach ( $sites as $site ) {
-		$wp_admin_bar->remove_node( 'blog-' . $site->blog_id . '-n' );
-		$wp_admin_bar->remove_node( 'blog-' . $site->blog_id . '-c' );
+	if ( ! is_user_logged_in() || ! is_multisite() ) {
+		return;
 	}
 
-}
-add_action( 'admin_bar_menu', 'msl_remove_nodes', 999 );
-
-// if ( current_user_can( 'manage_network' ) ) {
-
-function my_api_custom_route_sites() {
-	// $page = $request['page'];
-	// $max_pages = $query->max_num_pages;
-	// $total = $query->found_posts;
-	// $posts = $query->posts;
-
-	$args = array(
-		'number'    => 30,
-	);
-
-	$sites        = get_sites( $args );
-	$data         = array();
-	$i            = 0;
-	$main_favicon = get_site_icon_url();
-
-	// Determine Mapped Domain.
-	if ( false === get_option( 'wu_custom-domain' ) ) {
-		$site_mapped_domain = 'No Mapped Domain';
+	// If user doesn't have more than 1 site, or can't manage network, don't display the Multisite List.
+	if ( ! current_user_can( 'manage_network' ) ) {
+		return;
 	} else {
-		$site_mapped_domain = get_option( 'wu_custom-domain' );
+
+		/**
+		 * Enqueues React static file for the App.
+		 */
+		function msl_scripts() {
+
+			// Build files.
+			$react_app_js  = plugin_dir_url( __FILE__ ) . 'msl/build/static/js/all_in_one_file.js';
+			$react_app_css = plugin_dir_url( __FILE__ ) . 'msl/build/static/css/all_in_one_file.css';
+
+			wp_enqueue_style( 'msl-styles', $react_app_css, array(), $version );
+			wp_enqueue_script( 'msl-js', $react_app_js, array(), $version, true );
+
+			wp_localize_script(
+				'msl-js',
+				'msl_site_info',
+				array(
+					'site_url' => site_url(),
+				)
+			);
+		}
+		add_action( 'admin_enqueue_scripts', 'msl_scripts' );
+
+
+		/**
+		 * Removes items from the site list items.
+		 */
+		function msl_remove_nodes( $wp_admin_bar ) {
+
+			$sites = get_sites();
+			foreach ( $sites as $site ) {
+				$wp_admin_bar->remove_node( 'blog-' . $site->blog_id . '-n' );
+				$wp_admin_bar->remove_node( 'blog-' . $site->blog_id . '-c' );
+			}
+
+		}
+		add_action( 'admin_bar_menu', 'msl_remove_nodes', 999 );
+
+
+		/**
+		 * Define the REST routes.
+		 */
+		function my_api_custom_route_sites() {
+
+			$args = array(
+				'number'    => 100,
+			// TODO Pagination
+			);
+
+			$sites        = get_sites( $args );
+			$data         = array();
+			$i            = 0;
+			$main_favicon = get_site_icon_url();
+			$site_mapped_domain =  get_option( 'wu_custom-domain' );
+			// TODO Determine WP Ultimo Mapped Domain.
+
+			foreach ( $sites as $site ) {
+
+					$data[ $i ]['site_id'] = $site->blog_id;
+					$data[ $i ]['name']    = $site->blogname;
+					$data[ $i ]['url']     = $site->siteurl;
+					$data[ $i ]['domain']  = $site->domain;
+					// TODO $data[ $i ]['mapped_domain'] = $site->$site_mapped_domain;
+					$data[ $i ]['favicon'] = get_site_icon_url( 270, $main_favicon, $site->blog_id );
+					$i++;
+			}
+			return $data;
+		}
+
+		/**
+		 * Register the site routes.
+		 */
+		function register_sites() {
+			register_rest_route(
+				'msl/v1',
+				'sites/',
+				//TODO 'sites/(?P<page>[1-9]{1,2})',
+				array(
+					'methods'  => 'GET',
+					'callback' => 'my_api_custom_route_sites',
+				)
+			);
+		}
+		add_action( 'rest_api_init', 'register_sites' );
+
 	}
-
-	foreach ( $sites as $site ) {
-
-			$data[ $i ]['site_id'] = $site->blog_id;
-			$data[ $i ]['name']    = $site->blogname;
-			$data[ $i ]['url']     = $site->siteurl;
-			$data[ $i ]['domain']  = $site->domain;
-			$data[ $i ]['mapped_domain'] = $site_mapped_domain;
-			$data[ $i ]['favicon'] = get_site_icon_url( 270, $main_favicon, $site->blog_id );
-			$i++;
-	}
-	return $data;
 }
-
-function register_sites() {
-	register_rest_route(
-		'msl/v1',
-		'sites/',
-		// 'sites/(?P<page>[1-9]{1,2})',
-		array(
-			'methods'  => 'GET',
-			'callback' => 'my_api_custom_route_sites',
-			// 'args'     => array(
-			// 	'page' => array(
-			// 		'required' => true,
-			// 	),
-			// ),
-		)
-	);
-}
-add_action( 'rest_api_init', 'register_sites' );
-
-
+add_action( 'init', 'check_user' );
