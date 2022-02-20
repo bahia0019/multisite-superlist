a5efabdc85705081d73d3211141c765acc7105ba
William Bay <william@flauntyoursite.com>
2022-02-20 14:36:01 -0800

    Updated changes for WP Plugin review.

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Multisite SuperList
 
-> Replace that useless WordPress Multisite site list with something far better!
+> Replace that useless WordPress Multisite My Sites list with something far better!
 
 - If you manage lots of sites in a Multisite network, you'll know how frustrating using the Sites List can be. You can't scroll, so if you have more than 30 sites, you are forced to go to the Sites screen. The flyout submenus can be bit hard to navigate, and there are options in there that are kinda pointless (comments???).
-
 - That said, the Sites List has a lot of potential, and I wanted to improve upon it. The ideas I had were:
 - It should have useful links without submenus.
 - It should have a search feature like the very cool plugin [My Sites Search](https://github.com/trepmal/my-sites-search) by [Kailey Lampert](https://github.com/trepmal)
@@ -16,7 +15,8 @@
 
 ## Installation
 
-- This is a WordPress plugin that will only work in a Multisite environment (if you don't know what that means, you don't have it).
+- This is a WordPress plugin that will only work with Multisite.
+- Download the ZIP file from this page, and go install through the Plugins section of your WordPress Admin area. 
 
 ### Download the zip file
 
@@ -35,6 +35,11 @@
 
 All notable changes to this project will be documented in this file.
 
+## [2.1.1]
+
+### Changed
+
+-Revisions for plugin review team.
 
 ## [2.1.0]
 
--- multisite-superlist.php
+++ multisite-superlist.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Multisites SuperList
  * GitHub Plugin URI: https://github.com/bahia0019/multisite-superlist
  * Description:       Replaces My Sites list with a scrollable/searchable list of Sites.
- * Version:           2.1.0
+ * Version:           2.1.1
  * Author:            William Bay | Flaunt Your Site
  * Author URI:        http://flauntyoursite.com
  * License:           GPL-2.0+
@@ -17,12 +17,12 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-$version = '2.1.0';
+$version = '2.1.1';
 
 /**
  * Checks user to determine if plugin should run.
  */
-function check_user() {
+function mssl_check_user() {
 
 	if ( ! is_user_logged_in() || ! is_multisite() ) {
 		return;
@@ -36,7 +36,7 @@ function check_user() {
 		/**
 		 * Enqueues React static file for the App.
 		 */
-		function msl_scripts() {
+		function mssl_scripts() {
 
 			// Build files.
 			$react_app_js  = plugin_dir_url( __FILE__ ) . 'msl/build/static/js/all_in_one_file.js';
@@ -53,35 +53,20 @@ function check_user() {
 				)
 			);
 		}
-		add_action( 'admin_enqueue_scripts', 'msl_scripts' );
-
-		// /**
-		// * Removes items from the site list items.
-		// */
-		// function msl_remove_nodes( $wp_admin_bar ) {
-
-		// $sites = get_sites();
-		// foreach ( $sites as $site ) {
-		// $wp_admin_bar->remove_node( 'blog-' . $site->blog_id . '-n' );
-		// $wp_admin_bar->remove_node( 'blog-' . $site->blog_id . '-c' );
-		// }
-
-		// }
-		// add_action( 'admin_bar_menu', 'msl_remove_nodes', 999 );
+		add_action( 'admin_enqueue_scripts', 'mssl_scripts' );
 
 		/**
 		 * Remove the default WP Admin Bar My Sites menu.
-		 *
-		 * @return void
+		 * Thanks to Per Søderlind.
 		 */
-		function action_add_admin_bar_menus() {
+		function mssl_action_add_admin_bar_menus() {
 			remove_action( 'admin_bar_menu', 'wp_admin_bar_my_sites_menu', 20 );
 		}
 
 		/**
 		 * Define the REST routes.
 		 */
-		function my_api_custom_route_sites() {
+		function mssl_api_custom_route_sites() {
 
 			$args = array(
 				'number' => 100,
@@ -111,19 +96,19 @@ function check_user() {
 		/**
 		 * Register the site routes.
 		 */
-		function register_sites() {
+		function mssl_register_sites() {
 			register_rest_route(
 				'msl/v1',
 				'sites/',
 				// TODO 'sites/(?P<page>[1-9]{1,2})',
 				array(
 					'methods'  => 'GET',
-					'callback' => 'my_api_custom_route_sites',
+					'callback' => 'mssl_api_custom_route_sites',
 				)
 			);
 		}
-		add_action( 'rest_api_init', 'register_sites' );
+		add_action( 'rest_api_init', 'mssl_register_sites' );
 
 	}
 }
-add_action( 'init', 'check_user' );
+add_action( 'init', 'mssl_check_user' );
